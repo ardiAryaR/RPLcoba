953339b3e71b5a52c30897c4e11333b8e013dc7f
Ardi Arya Ramadhan <2300018163@webmail.uad.ac.id>
2026-04-19 17:09:02 +0700

    hdfaf

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -21,21 +21,3 @@ class Employee extends Model
         'role_id',
     ];
 }
-
-#apapalah
-class Employee extends Model
-{
-    use HasFactory;
-
-    protected $fillable = [
-        'name',
-        'email',
-        'phone_number',
-        'place_of_birth',
-        'date_of_birth',
-        'address',
-        'id_number',
-        'age',
-        'role_id',
-    ];
-}
